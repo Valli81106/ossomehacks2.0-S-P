2fabc191f724594a2c49d4878aeb7e7748f1c4b0
Haripriya <sistlasaiharipriya@gmail.com>
2025-01-21 23:05:38 +0530

    sponsers api

--- pages/api/sponsors/index.js
+++ pages/api/sponsors/index.js
@@ -1,11 +1,15 @@
-export default function handler(req, res) {
-    const mockSponsers = [
-        {name:"Sponser1"},
-        {name:"Sponder2"}
-    ]
-    if (req.method === 'GET') {
-      res.status(200).json(mockSponsers);
-    } else {
-      res.status(405).json({ error: 'Method Not Allowed' });
+import Sponsor from "@/utils/models/sponser.model";
+import DBInstance from "@/utils/db";
+export default async function handler(req, res) {
+  await DBInstance();
+  if (req.method === 'GET') {
+    try {
+      const sponsers = await Sponsor.find();
+      res.status(200).json({ success: true, data: sponsers });
+    } catch (e) {
+      res.status(500).json({ success: false, error: "Internal Server Error" });
     }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
+}
